cf270b631e4296abef8dfd1e8bdcd5dcf2e9bf18
Mary Nazareno <47845955+mgracnazareno@users.noreply.github.com>
2023-12-02 12:09:02 -0500

    Initial Commit

---
+++ ProjetFinal/src/main/java/com/agenda/projetfinal/ProjetFinalApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.projetfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetFinalApplication.class, args);
+    }
+
+}
